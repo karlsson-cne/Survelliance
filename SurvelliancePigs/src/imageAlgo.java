d8c7cb1e097af77af8a8492dbf33b360e9928e20
karlsson-cne <karlsson.cne@gmail.com>
2015-03-11 17:46:27 +0100

    Fixade så mat objekt kan användas för att representera bilderna. Hue, sat och val är nu olika objekt.

--- SurvelliancePigs/src/imageAlgo.java
+++ SurvelliancePigs/src/imageAlgo.java
@@ -26,10 +26,7 @@ public class imageAlgo {
 	//check all values of red with all sat and val.
 	public void findRedCluster()
 	{
-		
-		
-		 
-		
+				
 	}
 	
 	public void detectDeadPig()
--- SurvelliancePigs/src/main.java
+++ SurvelliancePigs/src/main.java
@@ -1,4 +1,5 @@
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -16,7 +17,7 @@ public class main {
 		//BEhöver ändra double till mat. Använda mat(int row, int col, int type)
 		// ta reda på vilken type det skall vara för 3 channel färgbild
 		// använd sedan put() för att lägga till.
-		double [] d = new double [3];
+		/*double [] d = new double [3];
 		double [][] hue = new double [m.cols()][m.rows()];
 		double [][] sat = new double [m.cols()][m.rows()];
 		double [][] val = new double [m.cols()][m.rows()];
@@ -33,12 +34,29 @@ public class main {
 			
 			
 		}
+		*/
+		// Gör om till Mat ist för double
+		Mat hue = new Mat(m.rows(),m.cols(),CvType.CV_8UC1);
+		Mat sat = new Mat(m.rows(),m.cols(),CvType.CV_8UC1);
+		Mat val = new Mat(m.rows(),m.cols(),CvType.CV_8UC1);
+		
+		for(int i = 0; i<m.cols(); i++)
+		{
+			for(int j = 0; j<m.rows(); j++)
+			{
+				hue.put(j, i, m.get(j, i));
+				sat.put(j, i, m.get(j, i));
+				val.put(j, i, m.get(j, i));
+			}
+		}
 		
 		
 		
-		System.out.println("At pos 0: Hue = "+ hue[0][0] + " Sat = "+ sat[0][0] + " Val = "+ val[0][0]);
+		//System.out.println("At pos 0: Hue = "+ hue[0][0] + " Sat = "+ sat[0][0] + " Val = "+ val[0][0]);
 		
-		//System.out.println("Mat: "+ m.col(100).dump());
+		System.out.println("Hue: "+ hue.dump());
+		System.out.println("Sat: "+ sat.dump());
+		System.out.println("Val: "+ val.dump());
 		
 		
 	}
